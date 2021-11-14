0eb0c20c785b86b9fa222aa37199188b9322c28f
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-11-14 16:48:20 +0100

    -Add bitmap resampling

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <boost/asio.hpp>
 #include <Xinput.h>
 #include "ViGEm/Client.h"
+#include <wingdi.h>
+#include <cstdio>
 
 #define BIT(n) (1U<<(n))
 
@@ -53,6 +55,127 @@ static void vAssert(VIGEM_ERROR e)
 	}
 }
 
+static char sbuf[4000000];
+
+static size_t align_up(size_t v, size_t al)
+{
+	size_t mod = v % al;
+	if (mod == 0)
+		return v;
+	else
+		return v - mod + al;
+}
+
+class Img
+{
+	size_t m_w;
+	size_t m_h;
+	uint8_t *m_data;
+
+	static size_t computeStride(size_t w)
+	{
+		return align_up(w * 3, 4);
+	}
+
+public:
+	Img(const char *filename)
+	{
+		FILE *f = fopen(filename, "rb");
+		if (f == nullptr)
+			throw std::runtime_error(filename);
+		fseek(f, 0x0A, SEEK_SET);
+		uint32_t off;
+		assert(fread(&off, sizeof(off), 1, f) == 1);
+		fseek(f, 0x0E, SEEK_SET);
+		BITMAPINFOHEADER h;
+		assert(fread(&h, sizeof(h), 1, f) == 1);
+		m_w = h.biWidth;
+		m_h = h.biHeight;
+		m_data = new uint8_t[m_w * m_h * 3];
+		if (h.biBitCount != 24)
+			throw std::runtime_error("BPP must be 24");
+
+		size_t s = computeStride(m_w);
+		size_t ds = m_w * 3;
+		for (size_t i = 0; i < m_h; i++) {
+			fseek(f, off + s * i, SEEK_SET);
+			assert(fread(m_data + ds * i, ds, 1, f) == 1);
+		}
+		fclose(f);
+		//std::printf("%zu, %zu\n", m_w, m_h);
+	}
+	Img(size_t w, size_t h) :
+		m_w(w),
+		m_h(h),
+		m_data(new uint8_t[m_w * m_h * 3])
+	{
+	}
+	~Img(void)
+	{
+		delete[] m_data;
+	}
+
+	void out(const char *filename)
+	{
+		FILE *f = fopen(filename, "wb");
+		if (f == nullptr)
+			throw std::runtime_error(filename);
+
+		auto s = computeStride(m_w);
+		assert(fwrite("BM", 2, 1, f) == 1);
+		{
+			uint32_t b = 0x0E + sizeof(BITMAPINFOHEADER) + s * m_h;
+			assert(fwrite(&b, sizeof(uint32_t), 1, f) == 1);
+			b = 0;
+			assert(fwrite(&b, sizeof(uint16_t), 1, f) == 1);
+			assert(fwrite(&b, sizeof(uint16_t), 1, f) == 1);
+			b = 0x0E + sizeof(BITMAPINFOHEADER);
+			assert(fwrite(&b, sizeof(uint32_t), 1, f) == 1);
+		}
+
+		BITMAPINFOHEADER hi{
+			sizeof(hi),	// biSize
+			static_cast<int32_t>(m_w),	// biWidth
+			static_cast<int32_t>(m_h),	// biWidth
+			1,	// biPlanes
+			24,	// biBitCount
+			BI_RGB,	// biCompression
+			static_cast<uint32_t>(s * m_h),	// biSizeImage
+			1,	// biXPelsPerMeter
+			1,	// biYPelsPerMeter
+			0,	// biClrUsed
+			0	// biClrImportant
+		};
+		assert(fwrite(&hi, sizeof(hi), 1, f) == 1);
+
+		size_t ds = m_w * 3;
+		uint8_t row[s] {};
+		for (size_t i = 0; i < m_h; i++) {
+			std::memcpy(row, m_data + ds * i, ds);
+			assert(fwrite(row, s, 1, f) == 1);
+		}
+		fclose(f);
+	}
+
+	void sample(Img &dst)
+	{
+		static auto nr = [](size_t i, size_t s) {
+			return static_cast<double>(i) / static_cast<double>(s - 1);
+		};
+		static auto dnr = [](double v, size_t s) {
+			return static_cast<size_t>(v * static_cast<double>(s - 1));
+		};
+		static auto cv = [](size_t i, size_t ss, size_t ds) {
+			return dnr(nr(i, ss), ds);
+		};
+		class px { uint8_t data[3]; };
+		for (size_t i = 0; i < dst.m_h; i++)
+			for (size_t j = 0; j < dst.m_w; j++)
+				reinterpret_cast<px*>(dst.m_data)[i * dst.m_w + j] =
+					reinterpret_cast<px*>(m_data)[cv(i, dst.m_h, m_h) * m_w + cv(j, dst.m_w, m_w)];
+	}
+};
+
 int main(int argc, char **argv)
 {
 	argc--;
@@ -61,6 +184,11 @@ int main(int argc, char **argv)
 		std::printf("Expected one arg");
 		return 1;
 	}
+
+	Img src("./sample.bmp");
+	Img dst(400, 240);
+	src.sample(dst);
+	dst.out("./sample_out.bmp");
 
 	auto vc = vigem_alloc();
 	vAssert(vigem_connect(vc));
@@ -77,6 +205,7 @@ int main(int argc, char **argv)
 	auto addr = argv[0];
 	char lastbuf[8];
 	//uint32_t last_in = 0;
+
 	try {
 		boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address::from_string(addr), 69);
 		boost::asio::io_service ios;
@@ -85,10 +214,10 @@ int main(int argc, char **argv)
 		std::printf("Connected to 3DS %s!\n", addr);
 		while (true) {
 			char buf[8];
-			if (boost::asio::read(sock, boost::asio::buffer(buf)) != sizeof(buf)) {
+			/*if (boost::asio::read(sock, boost::asio::buffer(buf)) != sizeof(buf)) {
 				std::printf("Disconnected.\n");
 				break;
-			}
+			}*/
 			auto in = reinterpret_cast<uint32_t&>(buf[0]);
 			auto x = reinterpret_cast<int16_t&>(buf[4]);
 			auto y = reinterpret_cast<int16_t&>(buf[6]);
@@ -123,6 +252,8 @@ int main(int argc, char **argv)
 			}
 			std::memcpy(lastbuf, buf, sizeof(buf));
 			//last_in = in;
+
+			sock.write_some(boost::asio::buffer(sbuf));
 		}
 	} catch (boost::system::system_error &e) {
 		std::printf("ERR: %s\n", e.what());
